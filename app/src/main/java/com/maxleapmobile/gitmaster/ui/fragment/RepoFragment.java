3eaee8f4bf651e15c7d9ae4c288bc994ea2e8429
Reacoder <sampythoner@gmail.com>
2015-10-19 16:49:35 +0800

    add search activity framework.

---
+++ app/src/main/java/com/maxleapmobile/gitmaster/ui/fragment/RepoFragment.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2015-present, MaxLeapMobile.
+ * All rights reserved.
+ * ----
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.maxleapmobile.gitmaster.ui.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RepoFragment extends Fragment {
+
+    public static RepoFragment newInstance(){
+        RepoFragment fragment = new RepoFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView view = new TextView(getActivity());
+        view.setTextColor(Color.BLACK);
+        view.setTextSize(16);
+        view.setText("RepoFragment");
+        return view;
+    }
+}
